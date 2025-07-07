782c76dc396eecf2752d2b31a4bb54496940572c
eazyrr22 <ezrirot@gmail.com>
2025-07-07 09:40:42 +0300

    Delete JS/Riddle_Game directory

--- JS/Riddle_Game/classes/round.js
+++
@@ -1,9 +0,0 @@
-
-function Round(riddle,ply){
-    const start = Date.now();
-    riddle.ask();
-    const end = Date.now();
-    ply.recordTime(start,end);
-
-}
-export default Round;
--- JS/Riddle_Game/riddle/arrayOfRiddles.js
+++
@@ -1,4 +0,0 @@
-import riddle1 from './r1.js'
-import riddle2 from './r2.js'
-const riddelsArray =[riddle1,riddle2]
-export default riddelsArray
